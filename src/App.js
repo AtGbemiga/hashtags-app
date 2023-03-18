ff68d8b5adedfe08ef25409947fed14aab9210ca
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2023-03-18 16:23:04 +0100

    Add functionality. Find, Length, Difference workin

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactDOM  from "react-dom";
 //Logic
 
 // Display
-class InformationDisplay extends React.Component{
+/*class InformationDisplay extends React.Component{
   constructor(props) {
     super(props);
   }
@@ -15,8 +15,8 @@ class InformationDisplay extends React.Component{
       </>
     )
   }
-}
-class LogicBtn extends React.Component{
+}*/
+/*class LogicBtn extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
@@ -38,20 +38,23 @@ class LogicBtn extends React.Component{
   render() {
     return(
       <>
-        {/*<LogicArea Btn = {this.state.Btn} />*/}
+        
         <div onClick={(this.firstMethod, this.secondMethod)}>{this.state.Btn}</div>
       </>
     )
   }
-}
+}*/
 
 
 class FindDuplicates extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {inputValue: "", duplicateLength: 0}
+    this.state = {inputValue: "", duplicateLength: 0, deleteDuplicate: null, copyDuplicate: null}
     this.findDuplicates = this.findDuplicates.bind(this)
     this.duplicateLengthMethod = this.duplicateLengthMethod.bind(this)
+    this.deleteDuplicateMethod = this.deleteDuplicateMethod.bind(this)
+    this.copyDuplicateMethod = this.copyDuplicateMethod.bind(this)
+    
   }
     findDuplicates(event) {
       this.setState((state) => 
@@ -61,6 +64,7 @@ class FindDuplicates extends React.Component{
       }
       )
     }
+
     duplicateLengthMethod(event) {
       this.setState((state) => {
         const firstResult = event.target.value.split(" ")
@@ -69,14 +73,35 @@ class FindDuplicates extends React.Component{
         return {duplicateLength: <h3>{thirdResult}</h3>}
       })
     }
+    
+    deleteDuplicateMethod(event) {
+      this.setState((state) => {
+        const firstResult = event.target.value.split(" ")
+        
+        const fourthResult = firstResult
+        let uniqueChars = [...new Set(fourthResult)].join(" ");
+        return {deleteDuplicate: <h3>{uniqueChars}</h3>}
+      })
+    }
+    copyDuplicateMethod(event) {
+      this.setState((state) => {
+        const firstResult = event.target.value.split(" ")
+        
+        const fourthResult = firstResult
+        let uniqueChars = [...new Set(fourthResult)].join(" ");
+        return {copyDuplicate: <h3>{uniqueChars}</h3>}
+      })
+    }
   render() {
     return(
       <>
-        <textarea inputValue = {this.state.inputValue} duplicateLength = {this.state.duplicateLength} onChange = {e =>((this.findDuplicates(e), this.duplicateLengthMethod(e)))}></textarea>
+        <textarea inputValue = {this.state.inputValue} duplicateLength = {this.state.duplicateLength} onChange = {e =>((this.findDuplicates(e), this.duplicateLengthMethod(e), this.deleteDuplicateMethod(e), this.copyDuplicateMethod(e)))}></textarea>
         
         <DuplicatesContent inputValue = {this.state.inputValue} />
-        <DuplicatesLength  duplicateLengthMethod = {this.state.duplicateLength}/>
-        <DeleteDuplicate />
+        <DuplicatesLength duplicateLength = {this.state.duplicateLength} />
+        {/*<h3  >{this.state.duplicateLength}</h3> This shows length functionality*/}
+        <DeleteDuplicate deleteDuplicateMethod = {this.state.deleteDuplicate} />
+        <CopyDuplicate copyDuplicateMethod = {this.state.copyDuplicate} />
       </>
     )
   }
@@ -89,7 +114,6 @@ class DuplicatesContent extends React.Component{
   render() {
     return(
       <>
-        
         {this.props.inputValue}
       </>
     )
@@ -100,12 +124,14 @@ class DuplicatesLength extends React.Component{
   constructor(props) {
     super(props)
   }
+  
   render() {
     return(
-      <>
+      <div>
+        <h4>issues found</h4>
+        <p>{this.props.duplicateLength}</p>
         
-        {this.props.duplicateLengthMethod}
-      </>
+      </div>
     )
   }
 }
@@ -114,18 +140,66 @@ class DeleteDuplicate extends React.Component{
   constructor(props) {
     super(props);
   }
+  render() {
+    return (
+      <>
+        {this.props.deleteDuplicateMethod}
+      </>
+    )
+  }
 }
 
-class App extends React.Component{
+class CopyDuplicate extends React.Component{
   constructor(props) {
     super(props);
   }
   render() {
     return (
+      <>
+        {this.props.copyDuplicateMethod}
+      </>
+    )
+  }
+}
+
+/*class SectionOne extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <h2>Hashtag Updates</h2>
+        
+        <DuplicatesLength />
+      </div>
+    )
+  }
+}
+
+class SectionTwo extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <h2>Type in Hashtags here</h2>
+        <FindDuplicates />
+      </div>
+    )
+  }
+}*/
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
       <div className="d-flex justify-content-between">
         <FindDuplicates />
-        <DuplicatesContent />
-        <DuplicatesLength />
         
       </div>
     )
